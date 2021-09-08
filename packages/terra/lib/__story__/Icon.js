1ce81a2e887cfe3673764838ae674e56262e125a
isabellaWang0108 <wangxbella0108@gmail.com>
2021-09-08 15:55:13 -0400

    complete icon page structure

--- packages/terra/lib/__story__/Icon.js
+++ packages/terra/lib/__story__/Icon.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames'
 import { Card } from '@sb-config/components/Card'
 import { SimpleTable, SimpleTableRow, SimpleTableCell } from '@sb-config/components/SimpleTable'
-
+import React from 'react';
 
 export const Iconsize = () => (
 <div style={{display:'flex', margin:60, justifyContent:'space-around'}}>
@@ -173,105 +173,105 @@ const content = {
   ],
   productIcons:[
     { name:'doc-filled', usage:"Symbol of documentation files." },
-    { name:'download', usage:"" },
-    { name:'file-v1', usage:"" },
-    { name:'file-v2', usage:"" },
-    { name:'information', usage:"" },
-    { name:'pdf-filled', usage:"" },
-    { name:'upload', usage:"" },
-    { name:'xls-filled', usage:"" },
+    { name:'download', usage:"Starts download files." },
+    { name:'file-v1', usage:"Indicates a folder." },
+    { name:'file-v2', usage:"Indicates a checklist." },
+    { name:'information', usage:"Indicates deal information." },
+    { name:'pdf-filled', usage:"Denotes, displays, or initiates a PDF file." },
+    { name:'upload', usage:"Starts upload actions." },
+    { name:'xls-filled', usage:"Denotes, displays, or initiates a XLS file." },
     
-    { name:'back-arrow', usage:"" },
-    { name:'dots', usage:"" },
-    { name:'link-out', usage:"" },
-    { name:'list-expanded', usage:"" },
-    { name:'nav-down', usage:"" },
-    { name:'nav-left', usage:"" },
-    { name:'nav-right', usage:"" },
-    { name:'nav-up', usage:"" },
+    { name:'back-arrow', usage:"Navigates back to previous step. " },
+    { name:'dots', usage:"Navigation move." },
+    { name:'link-out', usage:"Links to a new view out of the current one." },
+    { name:'list-expanded', usage:"Shows menu items." },
+    { name:'nav-down', usage:"Navigates downward." },
+    { name:'nav-left', usage:"Navigates to the left" },
+    { name:'nav-right', usage:"Navigates to the right." },
+    { name:'nav-up', usage:"Navigates upward." },
     
-    { name:'calendar', usage:"" }, 
-    { name:'clock', usage:"" }, 
-    { name:'connect', usage:"" }, 
-    { name:'encumbrance', usage:"" }, 
-    { name:'financial', usage:"" }, 
-    { name:'flame', usage:"" }, 
-    { name:'floorplan', usage:"" }, 
-    { name:'hourglass', usage:"" },
+    { name:'calendar', usage:"Displays a calendar or event, or performs a related action." }, 
+    { name:'clock', usage:"	Denotes or displays a clock or timer." }, 
+    { name:'connect', usage:"	Denotes or displays connection." }, 
+    { name:'encumbrance', usage:"Indicates an encumbrance." }, 
+    { name:'financial', usage:"	Denotes dollar or money related items." }, 
+    { name:'flame', usage:"Indicates Hot Prospect." }, 
+    { name:'floorplan', usage:"	Denotes or displays a clock or timer." }, 
+    { name:'hourglass', usage:"Shows pending status." },
     
-    { name:'key', usage:"" }, 
-    { name:'mag', usage:"" }, 
-    { name:'mail', usage:"" }, 
-    { name:'mobile', usage:"" }, 
+    { name:'key', usage:"Indicates the field or application has encryption." }, 
+    { name:'mag', usage:"Denotes or enters a search mode." }, 
+    { name:'mail', usage:"	Denotes or displays Email. " }, 
+    { name:'mobile', usage:"Denotes or displays phone number." }, 
     { name:'note', usage:"" }, 
-    { name:'notification', usage:"" }, 
-    { name:'paperclip', usage:"" }, 
-    { name:'picture', usage:"" },
+    { name:'notification', usage:"Sets or displays notifications." }, 
+    { name:'paperclip', usage:"	Denotes or shows attachment." }, 
+    { name:'picture', usage:"Indicates images." },
     
-    { name:'public-link', usage:"" }, 
-    { name:'reminder', usage:"" }, 
-    { name:'activity', usage:"" }, 
-    { name:'check', usage:"" }, 
-    { name:'close-v1', usage:"" }, 
-    { name:'close-v2', usage:"" }, 
-    { name:'collapse', usage:"" }, 
-    { name:'comment', usage:"" },
+    { name:'public-link', usage:"Sets or displays public link." }, 
+    { name:'reminder', usage:"Sets or displays a reminder.	" }, 
+    { name:'activity', usage:"Shows activities." }, 
+    { name:'check', usage:"Sets or displays confirmation." }, 
+    { name:'close-v1', usage:"Dismisses content like modals and alerts." }, 
+    { name:'close-v2', usage:"Dismisses or clears content. " }, 
+    { name:'collapse', usage:"Collapses a view in the current context. " }, 
+    { name:'comment', usage:"Writes a comment or shows the comment history. " },
     
-    { name:'exclamation-sign', usage:"" }, 
-    { name:'exclamation', usage:"" }, 
-    { name:'expand', usage:"" }, 
-    { name:'filter', usage:"" }, 
-    { name:'hide', usage:"" }, 
-    { name:'home', usage:"" }, 
-    { name:'info', usage:"" }, 
-    { name:'location', usage:"" },
+    { name:'exclamation-sign', usage:"Shows alert information." }, 
+    { name:'exclamation', usage:"Shows warning information. " }, 
+    { name:'expand', usage:"Expands a view in the current context. " }, 
+    { name:'filter', usage:"Displays or enters filter settings. " }, 
+    { name:'hide', usage:"Hides an item. " }, 
+    { name:'home', usage:"Indicates or displays a home screen.  " }, 
+    { name:'info', usage:"Shows information. " }, 
+    { name:'location', usage:"Denotes the concept of location or accesses the current geographic location." },
     
-    { name:'lock', usage:"" }, 
-    { name:'move', usage:"" }, 
-    { name:'move-v2', usage:"" }, 
-    { name:'pencil', usage:"" }, 
-    { name:'play', usage:"" }, 
-    { name:'plus-v2', usage:"" }, 
-    { name:'point', usage:"" }, 
-    { name:'preview', usage:"" },
+    { name:'lock', usage:"Stops changes, indicates security features. " }, 
+    { name:'move', usage:"Indicates or controls movements. " }, 
+    { name:'move-v2', usage:"Opens overflow menu items." }, 
+    { name:'pencil', usage:"Enters a edit mode." }, 
+    { name:'play', usage:"Begins or resumes media playback.	" }, 
+    { name:'plus-v2', usage:"Adds new items to current context. " }, 
+    { name:'point', usage:"Indicates status." }, 
+    { name:'preview', usage:"Shows preview, view count." },
     
-    { name:'question', usage:"" }, 
-    { name:'reset', usage:"" }, 
-    { name:'ribbon', usage:"" }, 
-    { name:'ribbon-filled', usage:"" }, 
-    { name:'set-up', usage:"" }, 
-    { name:'share', usage:"" }, 
-    { name:'sort-down', usage:"" }, 
-    { name:'sort-up', usage:"" },
+    { name:'question', usage:"Denotes or displays a question." }, 
+    { name:'reset', usage:"Starts reset action. " }, 
+    { name:'ribbon', usage:"Committed lease off." }, 
+    { name:'ribbon-filled', usage:"Committed lease on." }, 
+    { name:'set-up', usage:"Opens setting menu items." }, 
+    { name:'share', usage:"Shows share extensions, action extensions, and tasks." }, 
+    { name:'sort-down', usage:"Sorts down content." }, 
+    { name:'sort-up', usage:"Sorts up content." },
     
-    { name:'task', usage:"" }, 
-    { name:'star-filled', usage:"" }, 
-    { name:'star-outlined', usage:"" }, 
-    { name:'contact-info', usage:"" }, 
-    { name:'user', usage:"" }, 
-    { name:'users', usage:"" }, 
-    { name:'building-list', usage:"" }, 
-    { name:'building-small', usage:"" },
+    { name:'task', usage:"Shows or enters task items. " }, 
+    { name:'star-filled', usage:"Indicates a bookmarked item. " }, 
+    { name:'star-outlined', usage:"Indicates bookmark. " }, 
+    { name:'contact-info', usage:"Shows contact information" }, 
+    { name:'user', usage:"Denotes or displays user's information." }, 
+    { name:'users', usage:"Denotes or displays multiple uses' information." }, 
+    { name:'building-list', usage:"Indicates building portfolios. " }, 
+    { name:'building-small', usage:"Indicates a small propety." },
     
-    { name:'building', usage:"" }, 
-    { name:'officepark', usage:"" }, 
-    { name:'site-plan', usage:"" }, 
-    { name:'stack', usage:"" }, 
-    { name:'3d-tour', usage:"" }, 
-    { name:'download2', usage:"" }, 
-    { name:'download-multiple', usage:"" }, 
-    { name:'flexible-size-area', usage:"" },
+    { name:'building', usage:"Indicates a propety." }, 
+    { name:'officepark', usage:"Indicates multiple propeties." }, 
+    { name:'site-plan', usage:"Denotes or displays site plan." }, 
+    { name:'stack', usage:"Denotes or displays stacking plan." }, 
+    { name:'3d-tour', usage:"Indicates a 3d tour." }, 
+    { name:'download2', usage:"Starts downloading one asset." }, 
+    { name:'download-multiple', usage:"Starts downloading multiple assets." }, 
+    { name:'flexible-size-area', usage:"Indicates a flexible size area." },
     
-    { name:'link', usage:"" }, 
-    { name:'location', usage:"" }, 
-    { name:'tourbook', usage:"" }, 
-    { name:'tourbook-filled', usage:"" }, 
-    { name:'heart-outline', usage:"" }, 
-    { name:'heart', usage:"" }, 
-    { name:'union', usage:"" }, 
-    { name:'map-refresh', usage:"" },
+    { name:'link', usage:"Indicates a link." }, 
+    { name:'location', usage:"Denotes the concept of location or accesses the current geographic location." }, 
+    { name:'tourbook', usage:"Indicates add to tourbook option.  " }, 
+    { name:'tourbook-filled', usage:"Denotes or marks an item as saved in tourbook.	" }, 
+    { name:'heart-outline', usage:"Indicates save listing option. " }, 
+    { name:'heart', usage:"Denotes or marks a listing as saved" }, 
+    { name:'union', usage:"Shows a union icon. " }, 
+    { name:'map-refresh', usage:"Refreshes the current map view. " },
     
-    { name:'cube', usage:"" },
+    { name:'cube', usage:"Indicates a cube." },
   ]
 
 }
@@ -281,8 +281,8 @@ const content = {
 export const LogoIcons = () => (
 
   <div style={{display:'flex'}}>
-    {content.logoIcons.map((item)=>{
-      return  <div style={iconCardStyle}>
+    {content.logoIcons.map((item, i)=>{
+      return  <div style={iconCardStyle} key={i}>
        <br/>
        <span className={item.name + ' icon'}></span>
        <br/>
@@ -297,24 +297,39 @@ export const LogoIcons = () => (
 export const ProductIcons = () => (
 
   <div style={{display:'flex', flexWrap:'wrap'}}>
-    {content.productIcons.map((item)=>{
-      return  <div style={iconCardStyle}>
+    {content.productIcons 
+     .sort((a, b) =>{
+        if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if(a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+        return 0;
+       })
+     .map((item, i)=>{
+      return  <div style={iconCardStyle} key={i}>
        <br/>
        <span className={item.name + ' icon'}></span>
        <br/>
        <p className="body-3 black-060">{item.name}</p>
       </div>
-    })
+     })
     }
   </div>
 
 )
 
-export const LogoUsage = () => (
-  <Card>
-    <Card.Header>
-      <h3>Logos</h3>
-    </Card.Header>
+class Content extends React.Component {
+  state={input:'', content: content };
+
+  render() {
+    return <div>
+       <input style={{width:'98%', marginBottom:16, padding:12, borderRadius:5, border:'solid 1px #ccc'}}
+              type="text"
+              placeholder="Please enter a keyword to search"
+              onChange={(e)=>{ this.setState({input:e.target.value}) }}>
+       </input>
+       <Card>
+      <Card.Header>
+       <h3>Logos</h3>
+      </Card.Header>
 
     <Card.Content>
       <SimpleTable
@@ -324,8 +339,10 @@ export const LogoUsage = () => (
           { name: 'USAGE', width: 60 },
         ]}
       >
-       {content.logoIcons.map((item)=>{
-       return <SimpleTableRow>
+       {this.state.content.logoIcons
+       .filter(word=>word.name.includes(this.state.input))
+       .map((item, i)=>{
+       return <SimpleTableRow key={i}>
           <SimpleTableCell>
           <span className={item.name+" icon"} style={{fontSize:32}}></span>
           </SimpleTableCell>
@@ -341,9 +358,7 @@ export const LogoUsage = () => (
       </SimpleTable>
     </Card.Content>
   </Card>
-)
-
-export const ProductIconUsage = () => (
+  
   <Card>
     <Card.Header>
       <h3>Product icon</h3>
@@ -357,24 +372,39 @@ export const ProductIconUsage = () => (
           { name: 'USAGE', width: 60 },
         ]}
       >
-       {content.productIcons.map((item)=>{
-       return <SimpleTableRow>
-          <SimpleTableCell>
-          <span className={item.name+" icon"} style={{fontSize:32}}></span>
-          </SimpleTableCell>
-          <SimpleTableCell>
-            <p>{item.name}</p>
-          </SimpleTableCell>
-          <SimpleTableCell>
-           <p>{item.usage}</p>
-          </SimpleTableCell>
-        </SimpleTableRow>
+       {this.state.content.productIcons
+        .filter(word=>word.name.includes(this.state.input))
+        .sort((a, b) =>{
+          if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+          if(a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+          return 0;
+         })
+        .map((item, i )=>{
+          return <SimpleTableRow key={i}>
+            <SimpleTableCell>
+              <span className={item.name+" icon"} style={{fontSize:32}}></span>
+            </SimpleTableCell>
+            <SimpleTableCell>
+              <p>{item.name}</p>
+            </SimpleTableCell>
+            <SimpleTableCell>
+              <p>{item.usage}</p>
+            </SimpleTableCell>
+          </SimpleTableRow>
        }) }
 
       </SimpleTable>
     </Card.Content>
   </Card>
+    </div>;
+  }
+}
+export default Content;
+
+export const UsagePage = () => (
+  <Content/>
 )
+
 
 
 const iconCardStyle={backgroundColor:'#F7F7F7', width:100, height:100, textAlign:'center', marginRight:16, marginTop:16}
